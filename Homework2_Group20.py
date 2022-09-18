9577bfef11ca6f2b034760474b210387d569f637
reichmtm <reichmant@users.noreply.github.com>
2022-09-18 18:03:36 -0400

    Add BFS/DFS

--- Homework2_Group20.py
+++ Homework2_Group20.py
@@ -1,4 +1,8 @@
+import queue
+
 # Class for the Notes (or vertices) of our Graph
+
+
 class Location:
     def __init__(self, new_location):
         self.location = new_location
@@ -17,6 +21,8 @@ class Location:
         return self.adjacent_roads[road]
 
 # Class for the Graph of Romania
+
+
 class WeightedGraph:
     def __init__(self):
         self.cities = {}
@@ -38,12 +44,98 @@ class WeightedGraph:
         if end not in self.cities:
             self.add_city(end)
         # Add (both) roads to the Locations
-        #TODO: do we need both directions in the Graph?
+        # TODO: do we need both directions in the Graph?
         self.cities[start].add_road(self.cities[end], weight)
         self.cities[end].add_road(self.cities[start], weight)
 
     def get_cities(self):
         return self.cities.keys()
+
+
+def goalTest(current_city, goal_city):
+    if current_city.get_location() == goal_city.get_location():
+        return True
+    else:
+        return False
+
+
+def breadthFirstSearch(starting_city, goal_city):
+    # TODO: Solution is more akin to explored list - make it only the shortest path taken
+    print(starting_city.get_location() + ' --> ' + goal_city.get_location())
+    solution = queue.Queue()
+
+    solution.put(starting_city.get_location())
+
+    # Solution is complete if start/end city are the same
+    if goalTest(starting_city, goal_city):
+        return solution
+
+    frontier = queue.Queue()
+    frontier.put(starting_city)
+    explored = []
+
+    while True:
+        # Failure State, return empty path as solution
+        if frontier.empty():
+            return queue.Queue()
+
+        # Explore first city in frontier
+        current_city = frontier.get()
+        explored.append(current_city)
+
+        # Find neighboring cities that haven't been explored nor planning to be explored
+        for road in current_city.get_roads():
+            if road not in explored and road not in frontier.queue:
+                # TODO: Don't add to solution yet
+                solution.put(road.get_location())
+                # If it's the place we're looking for, we're done
+                if goalTest(road, goal_city):
+                    return solution
+
+                # Otherwise, it'll be explored
+                frontier.put(road)
+
+    # TODO: Performance metrics in terms of # explored
+    # TODO: Performance metrics in terms of distance in solution
+
+
+def depthFirstSearch(starting_city, goal_city):
+    # TODO: Use solution instead of explored
+    print(starting_city.get_location() + ' --> ' + goal_city.get_location())
+
+    explored = [starting_city]
+
+    # Solution is complete if start/end city are the same
+    if goalTest(starting_city, goal_city):
+        return explored
+
+    frontier = queue.LifoQueue()
+    frontier.put(starting_city)
+    explored = []
+
+    while True:
+        # Failure State, return empty path as solution
+        if frontier.empty():
+            return []
+
+        # Explore first city in frontier
+        current_city = frontier.get()
+        explored.append(current_city)
+
+        # Find neighboring cities that haven't been explored nor planning to be explored
+        for road in current_city.get_roads():
+            if road not in explored and road not in frontier.queue:
+                # If it's the place we're looking for, we're done
+                if goalTest(road, goal_city):
+                    explored.append(road)
+                    return explored
+
+                # Otherwise, it'll be explored
+                frontier.put(road)
+
+    # TODO: Performance metrics in terms of # explored
+    # TODO: Performance metrics in terms of distance in solution
+
 
 if __name__ == '__main__':
     # Build Graph
@@ -66,7 +158,7 @@ if __name__ == '__main__':
     romania.add_roads('Craiova', 'Pitesti', 138)
     romania.add_roads('Fagaras', 'Bucharest', 211)
     romania.add_roads('Pitesti', 'Bucharest', 101)
-    romania.add_roads('giurgiu', 'Bucharest', 90)
+    romania.add_roads('Giurgiu', 'Bucharest', 90)
     romania.add_roads('Bucharest', 'Urziceni', 85)
     romania.add_roads('Neamt', 'Iasi', 87)
     romania.add_roads('Urziceni', 'Hirsova', 98)
@@ -74,9 +166,21 @@ if __name__ == '__main__':
     romania.add_roads('Urziceni', 'Vaslui', 142)
     romania.add_roads('Hirsova', 'Eforie', 86)
 
+    goal_city = romania.cities['Bucharest']
+
     # Print all of the roads in Romania
     for city in romania:
-        for road in city.get_roads():
-            starting_city = city.get_location()
-            ending_city = road.get_location()
-            print(starting_city, ending_city, city.get_weight(road))
+        # for road in city.get_roads():
+        #     starting_city = city.get_location()
+        #     ending_city = road.get_location()
+        #     print(starting_city, ending_city, city.get_weight(road))
+
+        print(breadthFirstSearch(city, goal_city).queue)
+
+        for x in depthFirstSearch(city, goal_city):
+            print(x.get_location())
+
+# TODO: aStarSearch()
+# TODO: bestFirstSearch()
+
+# TODO: Comments on correctness/efficiency as outlined at top of page 2
